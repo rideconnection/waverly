97be9acc40c06ca4eb63dd1b97ccc90c4944630f
Chris Bloom <chrisbloom7@gmail.com>
2014-12-09 09:41:49 -0500

    swapping out .rvmrc for .ruby-version to avoid trust issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,9 +48,6 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :keep_releases, 20
 
 namespace :deploy do
-
-  after :updated, 'rvmrc:trust'
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
