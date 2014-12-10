50e72da9792c76f1b81fb8476be8efb91af53de0
Chris Bloom <chrisbloom7@gmail.com>
2014-12-10 08:50:40 -0500

    Fixing russian doll cache fragment in users index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,10 @@ module ApplicationHelper
     max_updated_at = User.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "users/all-#{count}-#{max_updated_at}"
   end
+
+  def cache_key_for_user(user, current_user)
+    user_updated_at = user.updated_at.try(:utc).try(:to_s, :number)
+    current_user_updated_at = current_user.updated_at.try(:utc).try(:to_s, :number)
+    "users/#{user.id}-#{user_updated_at}-#{current_user_updated_at}"
+  end
 end
