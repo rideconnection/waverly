99508fb6e175edfd9264c1520f3958f66ce5729d
Kevin Chambers <kchambers@rideconnection.org>
2015-04-03 17:12:27 -0700

    Update API to only upload the most recent version
    of each trip record.

--- app/models/uploaded_trip.rb
+++ app/models/uploaded_trip.rb
@@ -2,4 +2,6 @@ class UploadedTrip < ActiveRecord::Base
   belongs_to :upload
   
   validates_presence_of :prime_number
+
+  scope :current_versions, -> { where('created_at = (SELECT MAX(created_at) FROM uploaded_trips AS ut WHERE ut.trip_id = uploaded_trips.trip_id)') }
 end
